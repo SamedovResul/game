062c8574d34d43bc4a7df5a31b0c7814b5516320
Resul <68106627+SamedovResul@users.noreply.github.com>
2021-03-20 11:51:11 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,70 +1,51 @@
-const form = document.getElementById("form")
-const inpt = document.querySelectorAll('.input')
-const container = document.querySelector(".container")
-const gameLink = document.querySelector('.gamelink')
+const text = document.querySelectorAll('.text')
 const question = document.querySelector('.question')
-const label = document.querySelectorAll('.label')
-const text = document.querySelectorAll('.p2')
-console.log(text)
 const questionArray = ['Bunlardan hansı ətyeyən bitkidir?']
-
+question.innerHTML = questionArray
 const answerArray = {
     One:[ 'Milçəkqapan', 'Qızılgül','Palıd' ,'Pambıq'],
     Two:[ 'Yonca', 'Şehçiçəyi','Zanbaq' ,'Çaytikanı'],
     Three:[ 'Çobanyastığı', 'Qovaq ', 'Meymun fincanı' ,'Dəvədabanı'],
 }
 
+
 const questions =   Math.floor(Math.random() * 3);
 console.log()
-question.innerHTML = questionArray
 let answer = 0
-let trueAnswer = 0
+let trueAnswer;
 for (let i = 0; i < text.length; i++) {
+    console.log(text[i])
     if(0 == questions){
         text[0].innerHTML = answerArray.One[0]
         text[1].innerHTML = answerArray.One[1]
         text[2].innerHTML = answerArray.One[2]
         text[3].innerHTML = answerArray.One[3]
-        trueAnswer = 1 
+        trueAnswer = "Milçəkqapan" 
     }else if(1 == questions){
         text[0].innerHTML = answerArray.Two[0]
         text[1].innerHTML = answerArray.Two[1]
         text[2].innerHTML = answerArray.Two[2]
         text[3].innerHTML = answerArray.Two[3]
-        trueAnswer = 2
+        trueAnswer = "Şehçiçəyi" 
     }else if(2 == questions){
         text[0].innerHTML = answerArray.Three[0]
         text[1].innerHTML = answerArray.Three[1]
         text[2].innerHTML = answerArray.Three[2]
         text[3].innerHTML = answerArray.Three[3]
-        trueAnswer = 3
+        trueAnswer = 'Meymun fincanı'
     }
-    
 }
-
-
-
-
-
-for (let i = 0; i < label.length; i++) {
-    label[i].addEventListener('click', function(e){
-        let index = [i]
-        label[i].style.border = '3px solid black'
-        answer = Number(index) + 1
-        console.log(answer)
-        console.log(trueAnswer)
-    })
-
-}
-form.addEventListener('submit', function(e){
-    e.preventDefault()
-    if(answer == trueAnswer){
-        gameLink.style.display = 'block'
-        console.log(true)
-    }else{
-        answer = 0
-        // trueAnswer = 0
-        console.log(false)
-    }
-    
-})
+$(document).ready(function () {
+    $(".quiz-option").click(function () {
+        let value = $(this).children("label").text()
+        console.log(value);
+        if (value == trueAnswer) {
+            console.log(value)
+            $(".gamelink").fadeIn();
+            $(this).css("background-color", "green");
+        } else {
+            console.log(false)
+            $(".gamelink").fadeOut();
+        }
+    });
+});
